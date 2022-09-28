b6c121e8f6bf06d1c57bce295824c0e896a3ba52
azim <budazimbud@gmail.com>
2022-09-28 15:25:12 +0700

    loading while get product show spinner

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Row, Container, Col } from "reactstrap";
+import { Row, Container, Col, Button, Spinner } from "reactstrap";
 import CardProduct from "../components/CardProduct";
 import { getListProduct } from "../redux/product";
 function Product() {
@@ -11,14 +11,31 @@ function Product() {
   }, []);
 
   return (
-    <div className="px-5">
-      <Row className="mt-1  ">
-        {products.map((product) => (
-          <Col sm={3} key={product.id}>
-            <CardProduct product={product} />
-          </Col>
-        ))}
-      </Row>
+    <div className="px-5 py-2">
+        {
+            products.length > 0 ?
+            (
+                <Row className="mt-1  ">
+                {products.map((product) => (
+                  <Col sm={3} key={product.id}>
+                    <CardProduct product={product} />
+                  </Col>
+                ))}
+              </Row>
+            ) : 
+            (<div className="text-center">
+                <Spinner />
+            </div>)
+        }
+     
+      <div className="d-flex justify-content-center gap-4">
+            <Button color="primary">
+                {"<"}
+            </Button>
+            <Button color="primary">
+                {">"}
+            </Button>
+      </div>
     </div>
   );
 }
